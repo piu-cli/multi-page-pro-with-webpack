1f8eb7958552563b97c0f366d8996bddd8614107
罗圈圈 <luoquanquan@sunlands.com>
2018-08-05 22:06:37 +0800

    初始化 eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "airbnb-base",
+    rules: {
+      semi: [2, 'never'],
+      'import/no-extraneous-dependencies': 0,
+      'global-require': 0,
+      'comma-dangle': [2, 'never']
+    }
+};
