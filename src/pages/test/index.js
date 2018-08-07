b879c7fc748267a48251d0ced1d7e29371489f62
罗圈圈 <luoquanquan@sunlands.com>
2018-08-07 14:27:58 +0800

    区分环境后打包

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,8 +1,16 @@
+/**
+ * @Author: 罗圈圈
+ * @Date: 2018-08-07 11:22:39
+ * @Last Modified by: 罗圈圈
+ * @Last Modified time: 2018-08-07 11:22:39
+ */
+
 require('babel-polyfill')
 require('./style.less')
-const quanquan = 'balabala'
-console.log('123456789')
 
-const set = new Set([1,2,3,4])
+const quanquan = 'balabala'
+console.log(quanquan)
+
+const set = new Set([1, 2, 3, 4])
 
 console.log(set)
